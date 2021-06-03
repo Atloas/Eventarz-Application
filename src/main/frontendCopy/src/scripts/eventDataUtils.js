4d23e0ef6e395744bea8614f9b68421bb7346257
Atloas <miter010@gmail.com>
2021-06-03 16:20:39 +0200

    removed unneeded files

--- src/main/frontendCopy/src/scripts/eventDataUtils.js
+++
@@ -1,6 +0,0 @@
-export function processEventData(data, username) {
-  var joined = data.participants.some(participant => participant.username === username);
-  var organized = data.organizer.username === username;
-  var processedData = { ...data, organized: organized, joined: joined };
-  return processedData;
-}
